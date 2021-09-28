ea82c5588241620394e8e8ee4a9d6242923e2c15
sgonzalez <sgonzalezoyuela@gmail.com>
2021-09-28 13:43:23 -0400

    idp ext test cases (empty)

---
+++ model_identity_provider_ext_test.go
@@ -0,0 +1,27 @@
+package jossoappi
+
+import "testing"
+
+func TestAddIdLookup(t *testing.T) {
+
+	// 1. create empty idp dto
+
+	// 2. add id lookup
+
+	// 3. verify id lookups size and names
+
+}
+
+func TestRemoveIdLookup(t *testing.T) {
+
+	// 1. create empty idp dto
+
+	// 2. add id lookup
+
+	// 3. verify id lookups size and names
+
+	// 4. remove id lookup
+
+	// 5. verify id lookups size and names
+
+}
