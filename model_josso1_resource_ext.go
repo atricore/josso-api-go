eede3ef7e24a5023b45288e6588e37787211f11e
Sebastian <sgonzalezoyuela@gmail.com>
2021-11-02 15:28:00 -0400

    new josso1 resource activation utils

---
+++ model_josso1_resource_ext.go
@@ -0,0 +1,23 @@
+package jossoappi
+
+func (rs *JOSSO1ResourceDTO) NewActivation(name string) (ActivationDTO, error) {
+	var ac ActivationDTO
+	ac.AdditionalProperties = make(map[string]interface{})
+	ac.AdditionalProperties["@c"] = ".ServiceConnectionDTO"
+	ac.SetName(name)
+	rs.SetActivation(ac)
+
+	return ac, nil
+}
+
+func (rs *JOSSO1ResourceDTO) RemoveActivation() bool {
+	// Remove an element from : p.IdentityLookups
+
+	if rs.Activation == nil {
+		return false
+	}
+
+	rs.Activation = nil
+
+	return true
+}
